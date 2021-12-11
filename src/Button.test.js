49aac3b6250a3848a7f2c1fdd6fa4b7fefc6638f
Rafał Wojtkowiak <world@macbook-air-rafa.home>
2021-12-11 15:24:37 +0100

    Updated url

---
+++ src/Button.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button.js";
+
+it("should render a button element with a 'ui-button' class", () => {
+  render(<Button className="ui-button" />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement.classList.contains("ui-button")).toBe(true);
+});
+
+it("should have a disabled attribute when it receives a disabled prop", () => {
+  render(<Button disabled />);
+  const buttonElement = screen.getByRole("button");
+  expect(buttonElement.disabled).toBe(true);
+});
